818d677af39a323e46f1a49f27e12b0c2513dfa6
seongjin0902 <ksj05099@naver.com>
2023-07-12 15:47:23 +0900

    0712

---
+++ src/Tests/Main.java
@@ -0,0 +1,12 @@
+package Tests;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int val1 = 10;
+		int val2 = 20;
+		int val3 = 30;
+	}
+
+}
